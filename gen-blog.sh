9fe505a2f9909d01ae5b83b1ab7dc3551109b892
Mia G <mia.greer@mialikestech.dev>
2021-11-07 17:42:35 -0800

    New Homepage WIP

--- gen-blog.sh
+++ gen-blog.sh
@@ -25,7 +25,6 @@ genRenderer () {
         <meta charset=\"utf-8\">
         <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-        <link rel=\"stylesheet\" href=\"../../css/main.css\">
         <link rel=\"stylesheet\" href=\"../../css/blog-boilerplate.css\">
         <script src=\"https://cdn.jsdelivr.net/npm/marked/marked.min.js\"></script>
         <title>Blog - ${name}</title>
